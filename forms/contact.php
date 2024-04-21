e328c4c8e27307a49aa2f4ccb17900ae0e271d79
erika-hortencia <erika.cardoso@usp.br>
2024-04-20 23:31:56 -0300

    Email form adjust

--- forms/contact.php
+++ forms/contact.php
@@ -8,7 +8,7 @@
 
   if(isset($_POST['submit'])){
     $name = $_POST['name'];
-    $mailForm = $_POST['mail'];
+    $mailForm = $_POST['email'];
     $message = $_POST['message'];
 
     $mailTo = "erika.hp.cardoso@gmail.com";
